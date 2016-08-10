7ac824d8ec9ce0dbb3b0390ddd7c677f421d30f8
Alexander Stensrud <alex@stensrud.nu>
2016-08-10 21:03:22 +0200

    Supply an initial context

--- mux.go
+++ mux.go
@@ -1,23 +1,29 @@
 package keiro
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type Mux struct {
+	ctx    context.Context
 	router *httprouter.Router
 }
 
-func New() *Mux {
-	return &Mux{router: httprouter.New()}
+func Context(r *http.Request, name string) interface{} {
+	return r.Context().Value(name)
+}
+
+func New(ctx context.Context) *Mux {
+	return &Mux{ctx, httprouter.New()}
 }
 
 func (mux *Mux) Handle(method, path string, handler http.Handler) {
 	mux.router.Handle(method, path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		ctx := params(r.Context(), p)
-		r = r.WithContext(ctx)
+		r = r.WithContext(mux.ctx)
+		r = r.WithContext(params(r.Context(), p))
 		handler.ServeHTTP(w, r)
 	})
 }
