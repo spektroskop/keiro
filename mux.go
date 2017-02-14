5b5b704bcd8a4e5fa0adb1d0301fb3f99c1972ed
Alexander Stensrud <alex@stensrud.nu>
2017-02-14 17:27:18 +0100

    Update

--- mux.go
+++ mux.go
@@ -12,8 +12,16 @@ type Mux struct {
 	router *httprouter.Router
 }
 
-func Context(r *http.Request, key interface{}) interface{} {
-	return r.Context().Value(key)
+func WithValue(req *http.Request, key, object interface{}) *http.Request {
+	return req.WithContext(
+		context.WithValue(
+			req.Context(), key, object,
+		),
+	)
+}
+
+func Value(req *http.Request, key interface{}) interface{} {
+	return req.Context().Value(key)
 }
 
 func WithContext(ctx context.Context) *Mux {
@@ -34,3 +42,23 @@ func (mux *Mux) Handle(method, path string, handler http.Handler) {
 func (mux *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	mux.router.ServeHTTP(w, r)
 }
+
+func Compose(handlers ...func(http.Handler) http.Handler) func(http.Handler) http.Handler {
+	return func(handler http.Handler) http.Handler {
+		for i := len(handlers) - 1; i >= 0; i-- {
+			handler = handlers[i](handler)
+		}
+
+		return handler
+	}
+}
+
+func Into(hnd http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
+	return Compose(middleware...)(hnd)
+}
+
+func EmptyFunc(http.ResponseWriter, *http.Request) {
+
+}
+
+var Empty = http.HandlerFunc(EmptyFunc)
--- params.go
+++ params.go
@@ -7,15 +7,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type key int
+type contextKey string
 
-const paramsKey key = iota
+func (key contextKey) String() string {
+	return "keiro/" + string(key)
+}
+
+const ParamKey = contextKey("Param")
 
 func Param(r *http.Request, name string) string {
-	params := r.Context().Value(paramsKey)
+	params := r.Context().Value(ParamKey)
 	return params.(httprouter.Params).ByName(name)
 }
 
 func makeParams(ctx context.Context, params httprouter.Params) context.Context {
-	return context.WithValue(ctx, paramsKey, params)
+	return context.WithValue(ctx, ParamKey, params)
 }
